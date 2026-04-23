5c4a045f2c5fb80c8e6fa1cef2d3a5912ef23644
Bekzod Malikov <malikovbekzodman@gmail.com>
2026-04-23 14:04:45 +0500

    server changes

--- backend/lost_items/urls.py
+++ backend/lost_items/urls.py
@@ -21,5 +21,5 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
-    re_path(r'^(?!admin|auth|api|media|static).*$', TemplateView.as_view(template_name='index.html'), name='spa'),
+    re_path(r'^(?!admin/|auth/|api/|media/|static/).*$', TemplateView.as_view(template_name='index.html'), name='spa'),
 ]
