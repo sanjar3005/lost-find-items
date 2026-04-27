6f7e3889c7eb033361beb65c9e6d3cf682d3b9f4
Bekzod Malikov <malikovbekzodman@gmail.com>
2026-04-27 11:18:27 +0500

    admin panel fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,24 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/admin': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/static': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/media': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
